724ecc3ad33c5ace2f9282b6f37969de9fd70948
Dasha Buka <dvbuka@ucdavis.edu>
2023-09-28 23:16:02 -0700

    Added syntax highlighter.

--- src/rvCamera.gd
+++ src/rvCamera.gd
@@ -1,5 +1,4 @@
-class_name RvCamera
-extends Camera3D
+class_name RvCamera extends Camera3D
 
 # TODO:
 # - bounding box
@@ -22,52 +21,52 @@ var _target_position: Vector3 = self.position
 var _raw_input: Vector2 = Vector2.ZERO
 
 func _input(event):
-    # Disable input if the player is typing
-    if player_typing:
-        return
+	# Disable input if the player is typing
+	if player_typing:
+		return
 
-    if event.is_action_pressed("center_rv"):
-        center_rv()
-    
-    _raw_input = Input.get_vector(
-        "move_left",
-        "move_right",
-        "move_forward",
-        "move_back"
-    )
+	if event.is_action_pressed("center_rv"):
+		center_rv()
+	
+	_raw_input = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_forward",
+		"move_back"
+	)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    # Move the camera only when a new target position is detected
-    var move_dir = _get_move_dir()
-    if move_dir.length() > 0:
-        _target_position = self.position + move_dir
+	# Move the camera only when a new target position is detected
+	var move_dir = _get_move_dir()
+	if move_dir.length() > 0:
+		_target_position = self.position + move_dir
 
-    # Keep the camera within the boundary
-    if abs(_target_position.x) > boundary_width_half:
-        _target_position.x = self.position.x
+	# Keep the camera within the boundary
+	if abs(_target_position.x) > boundary_width_half:
+		_target_position.x = self.position.x
 
-    if abs(_target_position.z) > boundary_height_half:
-        _target_position.z = self.position.z
-               
-    self.position = self.position.lerp(_target_position, cam_speed * delta)
+	if abs(_target_position.z) > boundary_height_half:
+		_target_position.z = self.position.z
+		
+	self.position = self.position.lerp(_target_position, cam_speed * delta)
 
 func _get_move_dir() -> Vector3:
-    # Accommodate for cameras rotation
-    # Get the camera's forward direction
-    var cam_forward = -_transform.basis.z
-    # Negate y-axis influence
-    cam_forward.y = 0
-    cam_forward = cam_forward.normalized()
+	# Accommodate for cameras rotation
+	# Get the camera's forward direction
+	var cam_forward = -_transform.basis.z
+	# Negate y-axis influence
+	cam_forward.y = 0
+	cam_forward = cam_forward.normalized()
 
-    # Get cameras right direction
-    # (y-axis is already = 0 and basis is already normalized)
-    var cam_right = _transform.basis.x
+	# Get cameras right direction
+	# (y-axis is already = 0 and basis is already normalized)
+	var cam_right = _transform.basis.x
 
-    # Calculate the input direction in global space
-    var global_dir = cam_right * _raw_input.x + cam_forward * -_raw_input.y
-    return global_dir.normalized()
+	# Calculate the input direction in global space
+	var global_dir = cam_right * _raw_input.x + cam_forward * -_raw_input.y
+	return global_dir.normalized()
 
 func center_rv():
-    _target_position = _start_position
-    self.position = _start_position
+	_target_position = _start_position
+	self.position = _start_position
